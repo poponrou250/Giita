5c7b7d414f03d979188c03067cfde53b71299511
樋口裕貴 <sai102343@gmail.com>
2021-05-31 00:59:58 +0900

    投稿フォームの見た目を実装(簡易版)

---
+++ src/components/Post.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Post__wrapper">
+    <submit-form />
+  </div>
+</template>
+
+<script>
+export default {
+    import SubmitForm from "../components/SubmitForm.vue"
+}
+</script>
+
+<style scoped>
+.Post__wrapper {
+  margin: 0 auto;
+  max-width: 600px;
+  background-color: #ccc;
+}
+</style>
